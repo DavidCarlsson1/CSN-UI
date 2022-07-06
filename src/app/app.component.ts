07251fa7b131c8d73b4db96ada263c74f992d2bd
Carlsson David <David.Carlsson@csn.se>
2022-07-06 10:26:08 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { SiteVisionResponse } from './SiteVisionResponse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'prototype';
+export class AppComponent implements OnInit {
+  title = 'prototyp';
+
+  constructor(private _http: HttpClient) {}
+
+  public siteVisionResponse: SiteVisionResponse | undefined;
+
+  public ngOnInit(): void {
+    this._http
+      .get<SiteVisionResponse>(
+        'https://www.int14.csnnet.int/rest-api/1/1/4.23a46dfb173b37383332db4f/headless'
+      )
+      .subscribe({
+        next: (data: SiteVisionResponse) => {
+          this.siteVisionResponse = data;
+        },
+      });
+  }
 }
